b67c701a01ac17c3c24608941a47582dedbab951
Lincoln Mullen <lincoln@lincolnmullen.com>
2013-03-06 14:46:53 -0500

    Use png() instead of ggsave()

--- judaism/judaism.overview.R
+++ judaism/judaism.overview.R
@@ -24,43 +24,34 @@ credit <- function() {
 }
 
 # Data
-d <- read.csv("sarna.appendix.csv", comment.char = "#")
+data <- read.csv("sarna.appendix.csv", comment.char = "#")
 
-# Population estimates, 1660-2000
-png( file="jews.1660-2000.ggplot.png" )
-plot1 <- ggplot(d)
-plot1 + 
-geom_line(aes(d$year, d$estimate.low)) + 
-geom_line(aes(d$year, d$estimate.high)) +
-labs(title="American Jewish Population Estimates, 1660-2000", x = 
-     "Year", y="Population")
-makeFootnote("Data taken from Jonathan Sarna, American Judaism")
-dev.off
+# Population estimates
+# -------------------------------------------------------------------
+png(filename = "jewish-pop.old.png", width =2000, height=2000, res=300)
+ggplot(data) +
+geom_line(aes(data$year, data$estimate.low)) + 
+geom_line(aes(data$year, data$estimate.high)) +
+labs(title="American Jewish Population Estimates, 1660-2000",
+     x = "Year",
+     y="Population")
+makeFootnote("Data from Jonathan Sarna, American Judaism")
+dev.off()
 
-# Population estimates, 1776-1900
-png( file="jews.1776-1900.ggplot.png" )
-plot2 <- ggplot(d[3:13,])
-plot2 + 
-geom_line(aes(d$year[3:13], d$estimate.low[3:13])) + 
-geom_line(aes(d$year[3:13], d$estimate.high[3:13])) +
-labs(title="American Jewish Population Estimates, 1776-1900", x = 
-     "Year", y="Population")
-makeFootnote("Data taken from Jonathan Sarna, American Judaism")
-dev.off
-
-# Population estimates with bars
-# documentation: http://docs.ggplot2.org/0.9.3/geom_linerange.html
-# I think I have to offer it the midpoint, plus and minus the distance
-midpoint <- (d$estimate.high - d$estimate.low)/2 + d$estimate.low
-error <- (d$estimate.high - d$estimate.low)/2
-png(file="jews.1660-2000.errors.png", width = 900, height=900, 
-    res=100, pointsize=9)
-plot3 <- ggplot(d)
-plot3 + 
-geom_line(aes(d$year, midpoint)) +
-geom_linerange(aes(x=d$year, ymin = midpoint - error, ymax = 
-                   midpoint + error))+ 
-labs(title="American Jewish Population Estimates, 1660-2000", x = 
-     "Year", y="Population")
-makeFootnote("Data taken from Jonathan Sarna, American Judaism")
-dev.off
+# Population estimates with error bars
+# -------------------------------------------------------------------
+# Calculate mean/midpoint and the height of the error bars
+midpoint <- (data$estimate.high + data$estimate.low)/2 
+error <- (data$estimate.high - data$estimate.low)/2
+png(filename = "jewish-pop.png", width=2000, height=2000, res=300)
+ggplot(data) +
+geom_line(aes(data$year, midpoint)) +
+geom_errorbar(aes(
+                  x=data$year,
+                  ymin = midpoint - error,
+                  ymax = midpoint + error))+
+labs(title="American Jewish Population Estimates, 1660-2000",
+     x = "Year",
+     y="Population")
+makeFootnote("Data from Jonathan Sarna, American Judaism")
+dev.off()

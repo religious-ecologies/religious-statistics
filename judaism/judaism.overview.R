c9df9e4f8a6f9d09c27ab37c63fec83fedf50378
Lincoln Mullen <lincoln@lincolnmullen.com>
2013-03-06 15:53:33 -0500

    Plot 19th century

--- judaism/judaism.overview.R
+++ judaism/judaism.overview.R
@@ -12,24 +12,28 @@ source("../attribution.R")
 # Data
 data <- read.csv("sarna.appendix.csv", comment.char = "#")
 
-# Population estimates
+# Population estimates as line charts
 # -------------------------------------------------------------------
-png(filename = "jewish-pop.old.png", width =2000, height=2000, res=300)
+png(filename = "jewish-pop.old.png",
+    width =2000, height=2000, res=300)
 ggplot(data) +
 geom_line(aes(data$year, data$estimate.low)) + 
 geom_line(aes(data$year, data$estimate.high)) +
 labs(title="American Jewish Population Estimates, 1660-2000",
      x = "Year",
-     y="Population")
-credit("Data: Jonathan Sarna, American Judaism.")
+     y = "Population")
+attribution("Data: Jonathan Sarna, American Judaism.")
 dev.off()
+
+# Calculate mean/midpoint and the height of the error bars
+# -------------------------------------------------------------------
+midpoint <- (data$estimate.high + data$estimate.low)/2 
+error <- (data$estimate.high - data$estimate.low)/2
 
 # Population estimates with error bars
 # -------------------------------------------------------------------
-# Calculate mean/midpoint and the height of the error bars
-midpoint <- (data$estimate.high + data$estimate.low)/2 
-error <- (data$estimate.high - data$estimate.low)/2
-png(filename = "jewish-pop.png", width=2000, height=2000, res=300)
+png(filename = "jewish-pop.png",
+    width=2000, height=2000, res=300)
 ggplot(data) +
 geom_line(aes(data$year, midpoint)) +
 geom_errorbar(aes(
@@ -38,6 +42,38 @@ geom_errorbar(aes(
                   ymax = midpoint + error))+
 labs(title="American Jewish Population Estimates, 1660-2000",
      x = "Year",
-     y="Population")
+     y = "Population")
+attribution("Data: Jonathan Sarna, American Judaism.")
+dev.off()
+
+# Population estimates for 19th century
+# -------------------------------------------------------------------
+png(filename = "jewish-pop.early-19c.png",
+    width=2000, height=2000, res=300)
+ggplot(data[3:8,]) +
+geom_line(aes(data$year[3:8], midpoint[3:8])) +
+geom_errorbar(aes(
+                  x=data$year[3:8],
+                  ymin = midpoint[3:8] - error[3:8],
+                  ymax = midpoint[3:8] + error[3:8]))+
+labs(title="American Jewish Population Estimates, 1776-1840",
+     x = "Year",
+     y = "Population")
+attribution("Data: Jonathan Sarna, American Judaism.")
+dev.off()
+
+# Population estimates late 19th century
+# -------------------------------------------------------------------
+png(filename = "jewish-pop.late-19c.png",
+    width=2000, height=2000, res=300)
+ggplot(data[9:13,]) +
+geom_line(aes(data$year[9:13], midpoint[9:13])) +
+geom_errorbar(aes(
+                  x=data$year[9:13],
+                  ymin = midpoint[9:13] - error[9:13],
+                  ymax = midpoint[9:13] + error[9:13]))+
+labs(title="American Jewish Population Estimates, 1850-1900",
+     x = "Year",
+     y = "Population")
 attribution("Data: Jonathan Sarna, American Judaism.")
 dev.off()

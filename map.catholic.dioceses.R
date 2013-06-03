8b7039cafe931e9f1cca8454648f9e742772f2d3
Lincoln Mullen <lincoln@lincolnmullen.com>
2013-06-03 02:42:17 -0400

    Extend time range; use Canadian data

--- map.catholic.dioceses.R
+++ map.catholic.dioceses.R
@@ -14,11 +14,13 @@ if (file.exists("temp/catholic.dioceses.geocoded.csv")) {
   cat("We're using the already geocoded data.\n")
   cat("Delete temp/catholic.dioceses.geocoded.csv' to redo the geocoding.\n")
   geo <- read.csv("temp/catholic.dioceses.geocoded.csv")
-  } else {
-    raw <- read.csv("data/catholic.dioceses.csv", comment.char = "#")
-    geo <- transform(raw, geo = geocode(as.character(diocese)))
-    write.csv(geo, "temp/catholic.dioceses.geocoded.csv")
-  }
+} else {
+  raw.us <- read.csv("data/catholic.dioceses.us.csv", comment.char = "#")
+  raw.canada <- read.csv("data/catholic.dioceses.canada.csv", comment.char = "#")
+  raw <- rbind(raw.us, raw.canada)
+  geo <- transform(raw, geo = geocode(as.character(diocese)))
+  write.csv(geo, "temp/catholic.dioceses.geocoded.csv")
+}
 
 # Only the continental United States
 geo <- subset(geo, 25 < geo.lat & geo.lat < 50)
@@ -43,7 +45,7 @@ PlotDioceses <- function(plot.year) {
   # Map the dioceses. Dots for archdioceses will cover dioceses
   png(filename =
    paste("outputs/map.catholic.dioceses.", plot.year, ".png", sep = ""),
-   width=2000, height=2000, res=300)
+   width=1000, height=1000, res=300)
   plot <- map + 
   geom_point(data = dioceses, colour="red", size = 2,
    aes(x = geo.lon, y = geo.lat)) +
@@ -56,7 +58,7 @@ PlotDioceses <- function(plot.year) {
   }
 
 # Plot every decade since 1790, plus 1963 (the last year with data)
-years <- c(seq(1790, 1960, 10), 1963)
+years <- seq(1790, 2010, 10)
 for (year in years) {
   PlotDioceses(year)
 }

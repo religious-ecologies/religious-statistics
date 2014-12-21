a7ed87e62303aaf05cb5580fa4eee740d31e05cf
Lincoln Mullen <lincoln@lincolnmullen.com>
2014-12-20 21:13:58 -0500

    Geocode all the congregationalists
    Be aware that many of the geocoded results are incorrect and will need
    to be fixed.

--- R/congregationalist-yearbooks.R
+++ R/congregationalist-yearbooks.R
@@ -22,5 +22,18 @@ cities <- cities %>%
 data <- data %>%
   left_join(cities)
 
+data[data$city == "Burford and Paris", ]$lon <- -80.42914
+data[data$city == "Burford and Paris", ]$lat <- 43.10328
+data[data$city == "English River, Williamstown", ]$lon <- -93.18904
+data[data$city == "English River, Williamstown", ]$lat <- 50.63601
+data[data$city == "Esquesing", ]$lon <- -79.88752
+data[data$city == "Esquesing", ]$lat <- 43.54965
+data[data$city == "Fall River" & data$state == "RI", ]$lon <- -71.15505
+data[data$city == "Fall River" & data$state == "RI", ]$lat <- 41.70149
+data[data$city == "New Castle", ]$lon <- -70.71608
+data[data$city == "New Castle", ]$lat <- 43.07246
+data[data$city == "Oro, Innisfil, and Argus", ]$lon <- -79.61124
+data[data$city == "Oro, Innisfil, and Argus", ]$lat <- 44.51272
+
 write.csv(data, file = paste0(dir, "congregationalists-geocoded.csv"),
           row.names = FALSE)
